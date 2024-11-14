b1bf9d9af5ca725e5deb15dd860858ea63050568
culiiik <mr.pavel.novak@gmail.com>
2024-11-15 00:54:13 +0100

    e

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 tr -dc A-Za-z0-9 </dev/urandom | head -c 32 > ./zitadel-masterkey
 
-/var/www/zitadel start-from-init --masterkey "${ZITADEL_MASTERKEY}" --tlsMode disabled --externalDomain "zitadel" --masterkeyFile ./zitadel-masterkey
+/var/www/zitadel start-from-init --masterkey "${ZITADEL_MASTERKEY}" --tlsMode disabled --externalDomain "zitadel"
